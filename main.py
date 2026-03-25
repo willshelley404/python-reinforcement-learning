4285e21f2a0df78c46d181f831b76cd000abf47e
willshelley404 <willshelley404@gmail.com>
2026-03-25 13:17:54 -0400

    first pass with uv setup

---
+++ main.py
@@ -0,0 +1,13 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "requests>=2.33.0",
+#     "rich>=14.3.3",
+# ]
+# ///
+
+import sys
+from  rich import print
+import requests
+
+print(sys.version)
